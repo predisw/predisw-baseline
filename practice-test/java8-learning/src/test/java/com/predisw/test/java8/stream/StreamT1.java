955cdc3de5fdf6e10de51fa49eac3db12697a750
predisw <predisw@sina.com>
2017-10-18 23:25:54 +0800

    update streamT

--- practice-test/java8-learning/src/test/java/com/predisw/test/java8/stream/StreamT1.java
+++ practice-test/java8-learning/src/test/java/com/predisw/test/java8/stream/StreamT1.java
@@ -2,8 +2,11 @@ package com.predisw.test.java8.stream;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class StreamT1 {
@@ -24,5 +27,62 @@ public class StreamT1 {
     }
 
 
+    @Test
+    public void filterAndMapToInt(){
+
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        Stream<Integer> stream = numbers.stream();
+        stream.filter((x) -> {
+            return x % 2 == 0;
+        }).map(x -> {
+            return x * x;
+        }).mapToInt(x->x+1).sum();
+
+    }
+
+
+    @Test
+    public void forEachBreak(){
+
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        numbers.forEach(num->{
+            if(num>3){
+                System.out.println(num);
+               // break; // can't break in foreach !!
+            }
+        });
+
+    }
+
+    public static int getLength(String text) {
+        // Java 8
+        return Optional.ofNullable(text).map(String::length).orElse(-1);
+        // Pre-Java 8
+        // return if (text != null) ? text.length() : -1;
+    }
+
+    @Test
+    public void testLimitAndSkip() {
+        List<Person> persons = new ArrayList();
+        for (int i = 1; i <= 10; i++) {
+            Person person = new Person(i, "name" + i);
+            persons.add(person);
+        }
+        List<String> personList2 = persons.stream().
+                map(Person::getName).sorted().limit(6).collect(Collectors.toList());
+        System.out.println(personList2);
+    }
+    private class Person {
+        public int no;
+        private String name;
+        public Person (int no, String name) {
+            this.no = no;
+            this.name = name;
+        }
+        public String getName() {
+            System.out.println(name);
+            return name;
+        }
+    }
 
 }
