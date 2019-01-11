efb27bbda197818226e693bb80249859b7a01130
eggnwwg <guanjin.wang@ericsson.com>
2019-01-11 13:21:06 +0800

    http2-learning

---
+++ practice-test/http2-learning/src/test/java/com/predisw/test/http2/okhttp/GetExampleTest.java
@@ -0,0 +1,61 @@
+package com.predisw.test.http2.okhttp;
+
+import java.io.IOException;
+import org.junit.Test;
+
+public class GetExampleTest {
+
+    private void setupCustomTruststore(){
+        String truststore = "C:\\WORK\\work\\http2\\http2-client-trust.p12";
+        System.setProperty("javax.net.ssl.trustStore",truststore);
+        System.setProperty("javax.net.ssl.trustStorePassword","666666");
+        System.setProperty("javax.net.ssl.trustStoreType","PKCS12");
+    }
+
+
+    @Test
+    public void getOkhttpGithubServer() throws IOException {
+        GetExample example = new GetExample();
+        String response = example.runSimpleGet("https://raw.github.com/square/okhttp/master/README.md");
+        System.out.println(response);
+    }
+    @Test
+    public void getLocalhostHttp2Server() throws IOException {
+        setupCustomTruststore();
+        GetExample example = new GetExample();
+        String response = example.runSimpleGet("https://localhost:5000/");
+        System.out.println(response);
+    }
+
+    @Test
+    public void getSyncGet1() throws IOException {
+        GetExample example = new GetExample();
+        example.runSyncGet("https://publicobject.com/helloworld.txt");
+    }
+
+    @Test
+    public void getSyncGetToHttp2() throws IOException {
+        setupCustomTruststore();
+        GetExample example = new GetExample();
+        example.runSyncGet("https://localhost:5000/");
+    }
+
+    @Test
+    public void getAsyncGet1() throws IOException, InterruptedException {
+        GetExample example = new GetExample();
+        example.runAsyncGet("http://publicobject.com/helloworld.txt");
+        Thread.sleep(3000);
+
+    }
+
+
+    @Test
+    public void getAsyncGetToHttp2() throws IOException, InterruptedException {
+        setupCustomTruststore();
+        GetExample example = new GetExample();
+        example.runAsyncGet("https://localhost:5000/");
+        Thread.sleep(3000);
+    }
+
+
+}
